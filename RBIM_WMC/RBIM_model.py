55170961a865bbe48a0e92639971b29817bed64b
zarro110 <zarro110@gmail.com>
2026-09-23 15:32:59 +0200

    Make files
    Make the files
    make start of class

---
+++ RBIM_WMC/RBIM_model.py
@@ -0,0 +1,10 @@
+from wcnf_matrix import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class RBIM_class:
+    x = 69
+
+    def print_it(self):
+        print(self.x)
---
+++ RBIM_WMC/__main__.py
@@ -0,0 +1,11 @@
+
+from RBIM_model import RBIM_class
+
+
+
+if __name__ == "__main__":
+    print("ding")
+
+    RBIM = RBIM_class()
+
+    RBIM.print_it()
