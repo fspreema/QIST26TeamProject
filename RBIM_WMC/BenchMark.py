9cb47f877cfadd256b3124e2677f4debd7e98398
Iagomume <iagomume@gmail.com>
2026-09-25 18:18:16 +0200

    added more minor changes <3

--- RBIM_WMC/BenchMark.py
+++ RBIM_WMC/BenchMark.py
@@ -40,7 +40,7 @@ class BenchMark:
         results = []
         for m in models:
             tstart_raw = time.time()
-            true_value = m.ising_to_wcnf_matrix(m, beta).trace_formula()
+            true_value = self._Model.ising_to_wcnf_matrix(m, beta).trace_formula()
             tend_raw = time.time()
             result = tend_raw - tstart_raw
             results.append(result)
